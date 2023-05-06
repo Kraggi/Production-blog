a568c6dca03fbd223798df37cc4a739d39cf51b9
Kraggi <germanyanovich@gmail.com>
2023-05-06 15:40:26 +0300

    Добавление dev сервера и переменных окружения

--- config/build/buildWebpackConfig.ts
+++ config/build/buildWebpackConfig.ts
@@ -3,9 +3,10 @@ import { BuildOptions } from "./types/config";
 import { buildPlugins } from "./buildPlugins";
 import { buildLoaders } from "./buildLoaders";
 import { buildResolvers } from "./buildResolvers";
+import { buildDevServer } from "./buildDevServer";
 
 export function buildWebpackConfig(options: BuildOptions): webpack.Configuration {
-    const { paths, mode } = options;
+    const { paths, mode, isDev } = options;
 
     return {
         mode,
@@ -15,10 +16,12 @@ export function buildWebpackConfig(options: BuildOptions): webpack.Configuration
             path: paths.build,
             clean: true
         },
-        plugins: buildPlugins(options),
         module: {
             rules: buildLoaders()
         },
-        resolve: buildResolvers()
+        plugins: buildPlugins(options),
+        resolve: buildResolvers(),
+        devServer: isDev ? buildDevServer(options) : undefined,
+        devtool: isDev ? 'inline-source-map' : undefined,
     }
 }
