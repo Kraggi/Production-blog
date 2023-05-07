653a7787964bc50b0879c9e15fafd1cd67184ace
Kraggi <germanyanovich@gmail.com>
2023-05-07 16:30:14 +0300

    Добавление роутинга и ленивой загрузки для страниц

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
-import React from 'react';
-import { Counter } from "./components/Counter";
+import { Suspense } from "react";
+import { Link, Route, Routes } from "react-router-dom";
+import { AboutPageLazy } from "./pages/AboutPage/AboutPage.lazy";
+import { MainPageLazy } from "./pages/MainPage/MainPage.lazy";
 import './index.scss';
 
 export const App = () => {
     return (
         <div className='app'>
-            <Counter/>
+            <div>
+                <Link to='/'>Главная</Link>
+                <Link to='/about'>О сайте</Link>
+            </div>
+            <Suspense fallback={<h1>Загрузка...</h1>}>
+                <Routes>
+                    <Route path='/about' element={<AboutPageLazy/>}/>
+                    <Route path='/' element={<MainPageLazy/>}/>
+                </Routes>
+            </Suspense>
         </div>
     );
 };
