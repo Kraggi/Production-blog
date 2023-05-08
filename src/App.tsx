c3c9abc57021a99e77a75ee04eefefe09b303b86
Kraggi <germanyanovich@gmail.com>
2023-05-08 16:52:45 +0300

    Добавление тем и организация стилей

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,15 @@ import { Suspense } from "react";
 import { Link, Route, Routes } from "react-router-dom";
 import { AboutPageLazy } from "./pages/AboutPage/AboutPage.lazy";
 import { MainPageLazy } from "./pages/MainPage/MainPage.lazy";
-import './index.scss';
+import { useTheme } from "./theme/useTheme";
+import './styles/index.scss';
 
 export const App = () => {
+    const { theme, toggleTheme } = useTheme();
+
     return (
-        <div className='app'>
+        <div className={`app ${theme}`}>
+            <button onClick={toggleTheme}>Изменить тему</button>
             <div>
                 <Link to='/'>Главная</Link>
                 <Link to='/about'>О сайте</Link>
